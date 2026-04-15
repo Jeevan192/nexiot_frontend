5a2cf12fd774be4ca7d693459b372925102dedb8
Jeevan Reddy Madadi <jeevanreddy.cbit@gmail.com>
2026-04-15 23:15:50 +0530

    Production ready

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import legacy from '@vitejs/plugin-legacy'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    legacy({
+      targets: ['defaults', 'Android >= 6', 'iOS >= 11'],
+      modernPolyfills: true,
+      renderLegacyChunks: true,
+    }),
+  ],
   server: {
     port: 5173,
     proxy: {
